efe074bffadf9dbcdb58bdbf353615c1a75e9351
4aac <management.asher@gmail.com>
2025-08-17 12:10:32 +0200

    Added exercise 1

---
+++ E1/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String args[]) {
+
+        // Single line comment 
+        
+        /*
+         * Multiple line comment
+         */
+        
+         /**
+          * API comment
+          */
+
+        String variable_str = "Asier";
+        Integer variable_int = 18;
+        boolean variable_bool = true;
+
+        System.out.println(variable_str + variable_int + variable_bool);
+
+        System.out.println("Hello world!");
+    }
+    
+}
--- Eone/Eone.java
+++
@@ -1,6 +0,0 @@
-public class Eone {
-    public static void main(String args[]) {
-        System.out.println("Hello world!");
-    }
-    
-}
