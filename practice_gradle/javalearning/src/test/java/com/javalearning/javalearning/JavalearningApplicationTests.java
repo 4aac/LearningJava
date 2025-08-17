1001812e66586f4a425060ef9fbb49beaa69ed79
4aac <management.asher@gmail.com>
2025-08-17 11:56:43 +0200

    feat: restructured files

---
+++ practice_gradle/javalearning/src/test/java/com/javalearning/javalearning/JavalearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javalearning.javalearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavalearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
