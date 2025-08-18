1efd17a059ff2f19ba3a5d810f75851862f79f69
4aac <management.asher@gmail.com>
2025-08-19 00:10:23 +0200

    Added exercise 6

---
+++ E6/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+         * Java doesn't have variables by reference
+         */
+        
+        return;        
+    }
+    
+}
