f45466d3f7152d26ad3eedc505ebf941a1620d25
4aac <management.asher@gmail.com>
2025-08-18 22:49:13 +0200

    Finished exercise 5

--- E5/Main.java
+++ E5/Main.java
@@ -1,34 +1,44 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
-    public static void palindrome(String str1) {
-        int lenWord = length(str1);
-        for (int i=0; i < lenWord + 1; i++) {
-            if (palindrome[i] != palindrome[palindrome - i]) {
-                if (lenWord % 2 == 0) {
-                    return false;
-                }
-            }
+
+    public static boolean palindrome(String str) {
+        if (str == null) return false;
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) return false;
+            i++; j--;
         }
         return true;
     }
 
-    public static void anagram(String str1, String str2) {
-        int start = 0;
-        int next = 1;
-        int lenString = length(str2);
+    public static boolean anagram(String str1, String str2) {
+        if (str1 == null || str2 == null || str1.length() != str2.length()) return false;
+        int[] count = new int[256];
+        for (int i = 0; i < str1.length(); i++) {
+            // Here we use the ASCII value as the key of a HashMap
+            count[str1.charAt(i)]++;
+            count[str2.charAt(i)]--;
+        }
+        for (int c : count) if (c != 0) return false;
+        return true;
+    }
 
-        while (next <= lenString) {
-            
+    public static boolean isogram(String str) {
+        if (str == null) return false;
+        Set<Character> seen = new HashSet<>();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (!seen.add(c)) return false;
         }
         return true;
     }
 
-    public static void isogram(String str1) {
-        return true;
+    public static void main(String[] args) {
+        System.out.println(palindrome("radar"));
+        System.out.println(anagram("fried", "fired"));
+        System.out.println(isogram("asier"));
     }
-    
-    public static void main() {
-        palindrome("radar");
-        anagram("fried", "fired");
-        isogram("asier");
-   }
 }
+
