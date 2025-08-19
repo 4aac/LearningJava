92b6cd034c98bf684e058aeb172efb77ecd84d63
4aac <management.asher@gmail.com>
2025-08-19 15:02:30 +0200

    Added exercise 7

--- E7/Main.java
+++ E7/Main.java
@@ -1,6 +1,33 @@
 public class Main {
+
+    public static void recursive(int num) {
+        if (num >= 0) { 
+            System.out.println(num);
+            num--;
+            recursive(num);
+        }
+    } 
+
+    public static long factorial(long num) {
+        if (num == 0 || num == 1) {
+            return 1;
+        } else {
+            return num * factorial(num - 1);
+        }
+    }
+
+    public static int fibonacci(int num) {
+        if (num == 0 || num == 1) {
+            return 1;
+        } else {
+            return fibonacci(num - 1) + fibonacci(num - 2);
+        }
+    }
+
     public static void main(String[] args) {
-        
+        recursive(100);
+        System.out.println(factorial(20));
+        System.out.println(fibonacci(15));
     }
     
 }
